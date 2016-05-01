bbc31c9d7b6195f14597b4bc8cd5e8b31e6d136f
ricktam1469 <ricktam1469@iiitd.ac.in>
2016-05-02 05:08:01 +0530

    PCSMA PRoj 2nd push

--- pcsmaProject/app/src/main/java/com/example/ricktam/pcsmaproject/health.java
+++ pcsmaProject/app/src/main/java/com/example/ricktam/pcsmaproject/health.java
@@ -1,6 +1,5 @@
 package com.example.ricktam.pcsmaproject;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,9 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
-import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -28,59 +25,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
-public class Raspberry1 extends AppCompatActivity {
+public class health extends AppCompatActivity {
 
     AsyncTask<String, Void, String> task;
-    ProgressBar npb,ppb;
+
     private Handler mHandler;
-    int noise,pollution;
-    TextView ntext,ptext;
+
     Animation anim;
     LinearLayout l1,l2,l3;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_raspberry1);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        npb=(ProgressBar)findViewById(R.id.npb);
-        ppb=(ProgressBar)findViewById(R.id.ppb);
-
-        ntext=(TextView)findViewById(R.id.ntext);
-        ptext=(TextView)findViewById(R.id.ptext);
-
-
-
-        Button button = (Button) findViewById(R.id.health);
-
-        // Capture button clicks
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-
-                // Start NewActivity.class
-                Intent myIntent = new Intent(Raspberry1.this,
-                        health.class);
-                startActivity(myIntent);
-            }
-        });
-
-        anim = new AlphaAnimation(0.0f, 1.0f);
-        anim.setDuration(200); //You can manage the time of the blink with this parameter
-        anim.setStartOffset(20);
-        anim.setRepeatMode(Animation.REVERSE);
-        anim.setRepeatCount(Animation.INFINITE);
-
+        setContentView(R.layout.activity_health);
+         l1=(LinearLayout)findViewById(R.id.noise);
+        l2=(LinearLayout)findViewById(R.id.pollution);
+         l3=(LinearLayout)findViewById(R.id.pi);
         this.mHandler = new Handler();
 
         this.mHandler.postDelayed(m_Runnable, 5000);
@@ -96,24 +56,20 @@ public class Raspberry1 extends AppCompatActivity {
 
         {
             // Toast.makeText(Performance.this,"in runnable",Toast.LENGTH_SHORT).show();
-            task = new HttpAsyncTask1().execute("http://ec2-52-35-198-120.us-west-2.compute.amazonaws.com:3000/sensorData");
+            task = new HttpAsyncTask2().execute("http://ec2-52-35-198-120.us-west-2.compute.amazonaws.com:3000/health");
             //task2 = new HttpAsyncTask2().execute("http://ec2-52-35-198-120.us-west-2.compute.amazonaws.com:3000/health");
-            Raspberry1.this.mHandler.postDelayed(m_Runnable, 5000);
+            health.this.mHandler.postDelayed(m_Runnable, 5000);
         }
 
 
     };//runnable
-
-
-
-
 
     @Override
     protected void onStop(){
         super.onStop();
         mHandler.removeCallbacks(m_Runnable);
 
-        }
+    }
 
     public static String GET(String url)
     {
@@ -151,7 +107,7 @@ public class Raspberry1 extends AppCompatActivity {
         return result;
     }
 
-    private class HttpAsyncTask1 extends AsyncTask<String, Void, String>
+    private class HttpAsyncTask2 extends AsyncTask<String, Void, String>
     {
         @Override
         protected String doInBackground(String... urls)
@@ -173,49 +129,21 @@ public class Raspberry1 extends AppCompatActivity {
                 ////////////////////////////String Parsing////////////////////
                 //String str="{\"qid\":12,\"ques\":\"Question you know?\",\"timer\":1,\"answer\":\"NA\"}";
                 String[] strings = result.split(";");
-                String[] sensordata = strings[1].split("_");
-                String[] noiseData = sensordata[0].split("=");
-                String[] pollutionData = sensordata[1].split("=");
-                noise = Integer.parseInt(noiseData[1]);
-                pollution = Integer.parseInt(pollutionData[1]);
+                for(int i=0;i<strings.length;i++){
+                    String[] all=strings[i].split("=");
+                    if(all[0].equals("noiseSensor")&& all[1].equals("0")){
+                        l1.setBackgroundColor(Color.RED);
+                    }
+                    if(all[0].equals("pollutionSensor")&& all[1].equals("0")){
+                        l2.setBackgroundColor(Color.RED);
+                    }
 
-                // Toast.makeText(getBaseContext(), noise+"--"+pollution, Toast.LENGTH_LONG).show();
-                //ques = qt[1].substring(1, (qt[1].length() - 1));
-                // timer = Integer.parseInt(time[1]);
-                float n = (float)(((float)(noise) / (float)(npb.getMax()))) * 100;
-                float p = (float)((float)(pollution) / (float)((ppb.getMax()))) * 100;
-                //npb.setProgress(noise);
-                //ntext.setText((int) n);
-                Log.d("TAG","kajal-->  "+n);
-                String strProgress = n + " %";
-                npb.setProgress(noise);
-                              /* update secondary progress of horizontal progress bar */
-                ntext.setText(strProgress);
-                String strProgress1 = p + " %";
-                ppb.setProgress(pollution);
-                              /* update secondary progress of horizontal progress bar */
-                ptext.setText(strProgress1);
-
-
-
-
-                //ppb.setProgress(pollution);
-                //ptext.setText((int) p);
-
-                if(noise>12000)
-                {
-                    npb.setBackgroundColor(Color.RED);
-                    ntext.startAnimation(anim);
-
-                }
-                if(pollution>800){
-                    ppb.setBackgroundColor(Color.RED);
-                    ptext.startAnimation(anim);
                 }
 
             }
             catch (Exception e)
             {
+                l3.setBackgroundColor(Color.RED);
                 // Toast.makeText(getBaseContext(), "Nothing to Show", Toast.LENGTH_SHORT).show();
                 // timer = 0;
                 // ques = null;
@@ -241,8 +169,5 @@ public class Raspberry1 extends AppCompatActivity {
         }
     }
 
-/*
-
-*/
 
 }
